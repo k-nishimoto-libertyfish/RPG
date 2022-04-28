aa47f6e0e969419069a416ef7c3ae5f264326837
k_nishimoto <k_nishimoto@libertyfish.co.jp>
2022-04-28 13:16:11 +0900

    rpg step1 complete

--- rpg2.rb
+++ rpg2.rb
@@ -1,42 +1,32 @@
-#勇者クラス
 class Hero
-  #勇者の基本情報
   @@strength = 120
   @@power = 0
 
-  #勇者の体力を取得するメソッド
   def hero_strength
     @@strength
   end
 
-  #勇者の攻撃力を取得するメソッド
   def hero_power
     @@power = rand(10..25)
   end
 
-  #勇者が攻撃を受けた時の体力を減らすメソッド
   def hero_damage(monster_power)
     @@strength = @@strength - monster_power
   end
 end
 
-#モンスタークラス
 class Monster
-  #モンスターの基本情報
   @@strength = 100
   @@power = 0
 
-  #モンスターの体力を取得するメソッド
   def monster_strength
     @@strength
   end
 
-  #モンスターの攻撃力を取得するメソッド
   def monster_power
     @@power = rand(10..20)
   end
 
-  #モンスターが攻撃を受けた時の体力を減らすメソッド
   def monster_damage(hero_power)
     @@strength = @@strength - hero_power
   end
@@ -45,7 +35,6 @@ end
 puts "モンスターが現れた！戦闘を開始します！"
 
 while true
-  #インスタンス化
   hero = Hero.new
   hero_power = hero.hero_power
 
@@ -59,7 +48,6 @@ while true
 
   case choice_hero
   when 1
-    #勇者の攻撃のターン
     puts "勇者の攻撃! #{hero_power}のダメージを与えた"
     monster.monster_damage(hero_power)
     if monster.monster_strength <= 0
@@ -69,7 +57,6 @@ while true
       puts "モンスターの体力は残り #{monster.monster_strength}です！"
     end
     
-    #モンスターの攻撃のターン
     puts "モンスターの攻撃! #{monster_power}のダメージを受けた"
     hero.hero_damage(monster_power)
     if hero.hero_strength <= 0
@@ -78,7 +65,6 @@ while true
     else
       puts "勇者の体力は残り #{hero.hero_strength}です！"
     end
-  #逃げるを選択した場合
   when 2
     puts "無事に逃げれた！"
     break
